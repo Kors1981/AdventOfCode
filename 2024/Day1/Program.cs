02cb54c0012a94203a39afc6f8c3aed527f0119f
Stalidald <40857618+Stalidald@users.noreply.github.com>
2024-12-06 19:28:46 +0200

    Update Day1 solution

--- 2024/Day1/Program.cs
+++ 2024/Day1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+List<string> lines = File.ReadLines(Path.Combine(Directory.GetCurrentDirectory()+ $"..\\..\\..\\..\\..\\Inputs\\aocd1.txt")).ToList();
+Regex regex = new(@"(?<number>\d+)");
+int sum = 0;
+
+List<int> List1 = new();
+List<int> List2 = new();
+List<int> DiffList = new();
+List<int> MList = new();
+var count = lines.Count;
+for (int j = 0; j < count; j++)
+{
+    var res = lines[j].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    List1.Add(int.Parse(res[0]));
+    List2.Add(int.Parse(res[1]));
+}
+List1 = List1.OrderBy(x => x).ToList();
+List2 = List2.OrderBy(x => x).ToList();
+for (int j = 0; j < count; j++)
+{
+    DiffList.Add(Math.Abs(List1[j] - List2[j]));
+}
+sum = DiffList.Sum(x => x);
+Console.WriteLine($"Szumma 1A:{sum}");
+
+for (int j = 0; j < count; j++)
+{
+    var RunningNumber = List1[j];
+    MList.Add(List2.Where(x => x == RunningNumber).Count() * List1[j]);
+}
+
+sum = MList.Sum(x => x);
+Console.WriteLine($"Szumma 1B:{sum}");
+Console.ReadKey();
