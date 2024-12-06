4b93527b366e2fa22932338fb6b86254acd90273
Kors81 <mypersonalcontactpoint@gmail.com>
2024-12-06 20:18:00 +0200

    Day3 added

---
+++ 2024/Day3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+var fileloc = @"C:\Users\myper\OneDrive\AOC2024\AdventCodingDay1\aocd3.txt";
+var input = File.ReadAllText(fileloc);
+Regex regMul = new Regex(@"mul\((?<Number1>\d{1,3})\,(?<Number2>\d{1,3})\)");
+var matches = regMul.Matches(input);
+long result = 0;
+foreach (Match a in matches)
+{
+    result += int.Parse(a.Groups["Number1"].Value) * int.Parse(a.Groups["Number2"].Value);
+}
+Console.WriteLine($"Szumma 3A:{result}");
+result = 0;
+Regex regDoNot = new Regex(@"don\'t\(\)|do\(\)");
+var DoDontMatches = regDoNot.Matches(input);
+bool isFirst = true;
+foreach (Match a in matches)
+{
+    var Instruction = DoDontMatches.Where(x => x.Index < a.Index).LastOrDefault();
+    if (Instruction?.Value == "do()" || isFirst)
+    {
+        result += int.Parse(a.Groups["Number1"].Value) * int.Parse(a.Groups["Number2"].Value);
+    }
+    if (isFirst) isFirst = false;
+}
+Console.WriteLine($"Szumma 3B:{result}");
